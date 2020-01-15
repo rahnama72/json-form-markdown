62a37a3d4ffe5f18e91262fe7653059fd8031098
rahnama72 <45426057+rahnama72@users.noreply.github.com>
2020-01-15 09:33:29 +0330

    Add package manager

--- main.py
+++ main.py
@@ -102,3 +102,6 @@ def json_book_chapters_form_markdown(book_object):
     final_book_chapters.update({"h1s": final_h1_list})
 
     return final_book_chapters
+
+if __name__ == "__main__":
+    your example code goes here
